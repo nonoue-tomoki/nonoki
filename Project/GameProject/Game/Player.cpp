851545f74b34fb9ae64358767c3cd85403b53d1f
nonoue-tomoki <25051910@d.anabuki-college.net>
2025-12-04 03:18:47 +0900

    a

--- Project/GameProject/Game/Player.cpp
+++ Project/GameProject/Game/Player.cpp
@@ -1,5 +1,8 @@
 #include "Player.h"
 #include "Map.h"
+#include <cmath>
+#include "GLLibrary/CInput.h"
+#include "GLLibrary/CImage.h"
 
 enum {
     eAnimIdle = 0,
@@ -53,82 +56,191 @@ Player::Player(const CVector2D& p, bool flip) :
 
     m_img.SetSize(60, 60);
     m_img.SetCenter(30, 60);
-
     m_flip = flip;
-
     m_state = eState_Idle;
-
     m_is_ground = false;
+    m_wall_dir = 0;
+    m_jump_count = 0;
+    m_can_dash = true;
 
     m_rect = CRect(-16, -48, 16, 0);
 
-    m_img.AttachAnimationData(_anim_data, TILE_SIZE, TILE_SIZE);
+    m_img.AttachAnimationData(_anim_data, 60, 60);
     m_img.ChangeAnimation(eAnimIdle, true, 0, false);
 }
 
 Player::~Player() {
 }
 
+void Player::PlayerDash() {
+    if (!m_can_dash) return;
+
+    m_can_dash = false;
+
+    bool input_left = HOLD(CInput::eLeft);
+    bool input_right = HOLD(CInput::eRight);
+    bool input_up = HOLD(CInput::eUp);
+    bool input_down = HOLD(CInput::eDown);
+
+    CVector2D dash_vec(0, 0);
+
+    if (input_right) {
+        dash_vec.x = 1.0f;
+    }
+    else if (input_left) {
+        dash_vec.x = -1.0f;
+    }
+
+    if (input_up) {
+        dash_vec.y = -1.0f;
+    }
+    else if (input_down) {
+        dash_vec.y = 1.0f;
+    }
+
+    if (dash_vec.x != 0 || dash_vec.y != 0) {
+        float length = sqrtf(dash_vec.x * dash_vec.x + dash_vec.y * dash_vec.y);
+
+        if (length > 0.0f) {
+            dash_vec.x /= length;
+            dash_vec.y /= length;
+
+            dash_vec.x *= DASH_SPEED;
+            dash_vec.y *= DASH_SPEED;
+        }
+
+        m_vec = dash_vec;
+
+        if (m_vec.x > 0) m_flip = false;
+        if (m_vec.x < 0) m_flip = true;
+
+    }
+    else {
+        float dash_dir = m_flip ? -1.0f : 1.0f;
+        m_vec.x = dash_dir * DASH_SPEED;
+    }
+}
+
+
 void Player::StateIdle() {
-    const float move_speed = 4;
-    const float jump_pow = 10;
+    if (PUSH(CInput::eButton2)) {
+        PlayerDash();
+        m_state = eState_Jump;
+        return;
+    }
 
     if (HOLD(CInput::eLeft)) {
-        m_vec.x = -move_speed;
+        m_vec.x = -MOVE_SPEED;
         m_flip = true;
     }
     else if (HOLD(CInput::eRight)) {
-        m_vec.x = move_speed;
+        m_vec.x = MOVE_SPEED;
         m_flip = false;
     }
     else {
         m_vec.x = 0;
     }
 
-    if (m_is_ground && PUSH(CInput::eButton3)) {
-        m_vec.y = -jump_pow;
-        m_is_ground = false;
+    if (PUSH(CInput::eButton3)) {
+        m_vec.y = -JUMP_POWER;
         m_state = eState_Jump;
-        return;
-    }
-
-    if (!m_is_ground && m_vec.y > 0) {
-        m_state = eState_Jump;
+        m_jump_count = 1;
         return;
     }
 }
 
 void Player::StateJump() {
-    const float move_speed = 4;
-
-    if (HOLD(CInput::eLeft)) {
-        m_vec.x = -move_speed;
-        m_flip = true;
+    if (PUSH(CInput::eButton2)) {
+        PlayerDash();
+        return;
     }
-    else if (HOLD(CInput::eRight)) {
-        m_vec.x = move_speed;
-        m_flip = false;
+
+    if (std::abs(m_vec.x) < MOVE_SPEED) {
+        if (HOLD(CInput::eLeft)) {
+            m_vec.x = -MOVE_SPEED;
+            m_flip = true;
+        }
+        else if (HOLD(CInput::eRight)) {
+            m_vec.x = MOVE_SPEED;
+            m_flip = false;
+        }
+    }
+    else {
+        m_vec.x *= 0.995;
+    }
+
+    if (PUSH(CInput::eButton3) && m_jump_count < MAX_JUMP) {
+        m_vec.y = -JUMP_POWER;
+        m_jump_count++;
     }
 }
 
+void Player::StateWallGrab() {
+    m_vec.x = 0;
+        
+    if (PUSH(CInput::eButton3)) {
+        m_vec.x = -m_wall_dir * WALL_KICK_POWER_X;
+        m_vec.y = -WALL_KICK_POWER_Y;
+        m_state = eState_Jump;
+        m_wall_dir = 0;
+        m_jump_count = 1;
+        m_can_dash = true;
+        return;
+    }
+
+    if (HOLD(CInput::eButton1)) {
+        m_vec.y = 0;
+
+        if (HOLD(CInput::eUp)) {
+            m_vec.y = -WALL_SLIDE_SPEED * 1.5f;
+        }
+        else if (HOLD(CInput::eDown)) {
+            m_vec.y = WALL_SLIDE_SPEED * 2.0f;
+        }
+    }
+    else {
+        bool input_opposite = (m_wall_dir == 1 && HOLD(CInput::eLeft)) ||
+            (m_wall_dir == -1 && HOLD(CInput::eRight));
+
+        if (FREE(CInput::eButton1) || input_opposite || (m_wall_dir == 1 && FREE(CInput::eRight)) || (m_wall_dir == -1 && FREE(CInput::eLeft))) {
+            m_wall_dir = 0;
+            m_state = eState_Jump;
+            return;
+        }
+
+        m_vec.y += GRAVITY;
+
+        if (HOLD(CInput::eUp)) {
+            m_vec.y = -WALL_SLIDE_SPEED * 1.5f;
+        }
+        else if (HOLD(CInput::eDown)) {
+            m_vec.y = WALL_SLIDE_SPEED * 2.0f;
+        }
+    }
+
+    m_img.ChangeAnimation(eAnimJumpUp, false, 0, false);
+}
+
 void Player::StateDown() {
-    SetKill();
 }
 
 
 void Player::Update() {
     m_pos_old = m_pos;
 
-    if (m_state != eState_Down) {
-        m_vec.y += GRAVITY;
-    }
+    m_is_ground = false;
 
     switch (m_state) {
     case eState_Idle:
         StateIdle();
+        if (m_state != eState_Idle) m_vec.y += GRAVITY;
         break;
     case eState_Jump:
         StateJump();
+        m_vec.y += GRAVITY;
+        break;
+    case eState_WallGrab:
+        StateWallGrab();
         break;
     case eState_Down:
         StateDown();
@@ -136,30 +248,13 @@ void Player::Update() {
     }
 
     m_pos += m_vec;
-    
-    /*
-    m_scroll.x = m_pos.x - SCREEN_WIDTH / 2;
-    m_scroll.y = m_pos.y - SCREEN_HEIGHT / 2;
-    */
 
-    m_scroll = CVector2D(0, 0);
-
-    m_is_ground = false;
-
-    if (m_is_ground) {
-        if (m_vec.x != 0) {
-            m_img.ChangeAnimation(eAnimRun);
+    if (m_state == eState_Idle || m_state == eState_Jump) {
+        if (m_is_ground) {
+            m_img.ChangeAnimation(m_vec.x != 0 ? eAnimRun : eAnimIdle);
         }
         else {
-            m_img.ChangeAnimation(eAnimIdle);
-        }
-    }
-    else {
-        if (m_vec.y < 0) {
-            m_img.ChangeAnimation(eAnimJumpUp, false, 0, false);
-        }
-        else {
-            m_img.ChangeAnimation(eAnimJumpDown, false, 0, false);
+            m_img.ChangeAnimation(m_vec.y < 0 ? eAnimJumpUp : eAnimJumpDown, false, 0, false);
         }
     }
 
@@ -170,40 +265,51 @@ void Player::Draw() {
     m_img.SetPos(GetScreenPos(m_pos));
     m_img.SetFlipH(m_flip);
     m_img.Draw();
-    DrawRect();
 }
 
+
 void Player::Collision(Base* b) {
-    if (m_state == eState_Down) return;
-
     if (b->m_type == eType_Map) {
-
         if (Map* m = dynamic_cast<Map*>(b)) {
-            int collide_dir;
+            int t;
 
-            CVector2D pos_x_only = CVector2D(m_pos.x, m_pos_old.y);
-            collide_dir = m->CollisionRect(pos_x_only, m_rect);
-
-            if (collide_dir != 0) {
+            t = m->CollisionRect(CVector2D(m_pos.x, m_pos_old.y), m_rect);
+            if (t != 0) {
                 m_pos.x = m_pos_old.x;
                 m_vec.x = 0;
+
+                if (PUSH(CInput::eButton1)) {
+                    m_state = eState_WallGrab;
+                    m_wall_dir = t;
+                    m_flip = (t == 1);
+                    m_vec.y = 0;
+                    return;
+                }
+
+                if (m_state == eState_Jump) {
+                    if ((t == 1 && HOLD(CInput::eLeft)) || (t == -1 && HOLD(CInput::eRight))) {
+                        m_state = eState_WallGrab;
+                        m_wall_dir = t;
+                        m_flip = (t == 1);
+                        return;
+                    }
+                }
             }
 
-            CVector2D pos_y_only = CVector2D(m_pos.x, m_pos.y);
-            collide_dir = m->CollisionRect(pos_y_only, m_rect);
-
-            if (collide_dir != 0) {
+            t = m->CollisionRect(CVector2D(m_pos_old.x, m_pos.y), m_rect);
+            if (t != 0) {
                 m_pos.y = m_pos_old.y;
                 m_vec.y = 0;
-
                 if (m_vec.y >= 0) {
                     m_is_ground = true;
-                    if (m_state == eState_Jump) {
+                    m_jump_count = 0;
+                    m_wall_dir = 0;
+                    m_can_dash = true;
+                    if (m_state == eState_WallGrab || m_state == eState_Jump) {
                         m_state = eState_Idle;
                     }
                 }
             }
         }
-        return;
     }
 }
--- Project/GameProject/Game/Player.h
+++ Project/GameProject/Game/Player.h
@@ -3,9 +3,14 @@
 #include "GLLibrary/CInput.h"
 #include "GLLibrary/CImage.h"
 #include "GLLibrary/Utility.h"
+#include "Map.h"
 
-#define PLAYER_WIDTH (64)
-#define PLAYER_HEIGHT (64)
+#define DASH_SPEED 10
+#define WALL_SLIDE_SPEED 1
+#define MOVE_SPEED 4
+#define JUMP_POWER 10
+#define WALL_KICK_POWER_X 10
+#define WALL_KICK_POWER_Y 12
 
 class Player : public Base {
 private:
@@ -20,6 +25,7 @@ private:
     enum {
         eState_Idle,
         eState_Jump,
+        eState_WallGrab,
         eState_Down,
     };
     int m_state;
@@ -27,6 +33,12 @@ private:
     CImage m_img;
     bool m_flip;
     bool m_is_ground;
+    int m_wall_dir;
+    int m_jump_count;
+    const static int MAX_JUMP = 2;
+    bool m_can_dash;
+
+    void PlayerDash();
 
 public:
     Player(const CVector2D& pos, bool flip);
@@ -38,5 +50,6 @@ public:
 
     void StateIdle();
     void StateJump();
+    void StateWallGrab();
     void StateDown();
 };
